3742c95cd7d8921de59859ac1bdf803ceed4c2b0
Gabriella Vidal <gabriella.vidal@fiocruz.br>
2022-05-13 01:08:26 -0300

    novas funcionalidade

--- src/app/bolsa/bolsa.page.spec.ts
+++ src/app/bolsa/bolsa.page.spec.ts
@@ -2,19 +2,19 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
-import { HomePage } from './home.page';
+import { BolsaPage } from './bolsa.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('BolsaPage', () => {
+  let component: BolsaPage;
+  let fixture: ComponentFixture<BolsaPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [HomePage],
+      declarations: [BolsaPage],
       imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(BolsaPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
 
@@ -20,4 +20,5 @@ export class HomePage {
       document.body.setAttribute('color-theme', 'light')
     }
   }
+
 }
